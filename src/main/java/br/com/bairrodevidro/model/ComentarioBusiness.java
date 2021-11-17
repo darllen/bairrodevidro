4543b25357763944329c3a41711e18c8582d68f7
Jamilly Anunciada <jamillymilly10@hotmail.com>
2021-11-17 12:54:01 -0300

    Add files via upload

---
+++ src/main/java/br/com/bairrodevidro/model/ComentarioBusiness.java
@@ -0,0 +1,60 @@
+package main.java.br.com.bairrodevidro.model;
+
+
+import main.java.br.com.bairrodevidro.model.domain.Comentario;
+
+import javax.persistence.*;
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+public class ComentarioBusiness {
+    @PersistenceContext
+    public void criarComentario (Comentario opiniao) throws IOException {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("bairrodevidro");
+        EntityManager em = emf.createEntityManager();
+
+        Comentario novaOpiniao = new Comentario();
+
+        novaOpiniao.setIdComentario(opiniao.getIdComentario());
+        novaOpiniao.setComentario(opiniao.getComentario());
+        novaOpiniao.setIdUsuario(opiniao.getIdUsuario());
+        novaOpiniao.setIdBairro(opiniao.getIdBairro());
+        novaOpiniao.setIdBairro(opiniao.getIdBairro());
+        novaOpiniao.setTipo(opiniao.getTipo());
+
+        em.getTransaction().begin();
+        em.persist(novaOpiniao);
+        em.getTransaction().commit();
+
+        em.close();
+
+    }
+
+    public void listaComentario() {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("bairrodevidro");
+        EntityManager em = emf.createEntityManager();
+
+        try {
+            em.getTransaction().begin();
+            ArrayList<Comentario> listarComentario = (ArrayList<Comentario>) em
+                    .createQuery("SELECT u from Comentario u", Comentario.class).getResultList();
+//                    .createQuery("SELECT u from Comentario u where u.idBairro = :idBairro and u.tipo = :tipo" ,Comentario.class)
+//                    .setParameter("idBairro", comentario.getBairro())
+//                    .setParameter("tipo", comentario.getTipo()).getResultList();
+
+            System.out.println(listarComentario);
+        } catch (NoResultException e) {
+            System.out.println("Comentário não encontrado!");
+
+        }
+        //em.close();
+
+    }
+
+
+}
+
+
